0d6f41af045b8f9865607b2979e11e10dbca1ec7
Abe Massry <amassry@linode.com>
2019-03-29 19:51:24 -0400

    list and table view

--- main.go
+++ main.go
@@ -7,6 +7,12 @@ import (
 
 	ui "github.com/gizak/termui/v3"
 	"github.com/gizak/termui/v3/widgets"
+
+	"github.com/linode/linodego"
+	"golang.org/x/oauth2"
+
+	"net/http"
+	"os"
 )
 
 type View interface {
@@ -15,7 +21,7 @@ type View interface {
 }
 
 type NamedView struct {
-	Next *NamedView
+	Next          View
 	nameParagraph *widgets.Paragraph
 }
 
@@ -47,7 +53,7 @@ func (v *NamedView) HandleEvent(ctx context.Context, e ui.Event, ch chan<- View)
 		ch <- v.Next
 		return nil
 	}
-	
+
 	if v.nameParagraph.Text == "" {
 		v.nameParagraph.Text = e.ID
 	} else {
@@ -57,12 +63,163 @@ func (v *NamedView) HandleEvent(ctx context.Context, e ui.Event, ch chan<- View)
 	return nil
 }
 
+func getLinodes() []linodego.Instance {
+	apiKey, ok := os.LookupEnv("LINODE_TOKEN")
+	if !ok {
+		log.Fatal("Could not find LINODE_TOKEN, please assert it is set.")
+	}
+	tokenSource := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: apiKey})
+
+	oauth2Client := &http.Client{
+		Transport: &oauth2.Transport{
+			Source: tokenSource,
+		},
+	}
+
+	linodeClient := linodego.NewClient(oauth2Client)
+	linodeClient.SetDebug(false)
+
+	linodes, err := linodeClient.ListInstances(context.Background(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return linodes
+}
+
+type LinodeDetailView struct {
+	linode             linodego.Instance
+	parentView         View
+	tableWidget        *widgets.Table
+	instructionsWidget *widgets.Paragraph
+}
+
+func NewLinodeDetailView(parent View, i linodego.Instance) *LinodeDetailView {
+	tableWidget := widgets.NewTable()
+
+	tableWidget.Rows = [][]string{
+		[]string{"Label", i.Label},
+		[]string{"Status", string(i.Status)},
+		[]string{"Plan", i.Type},
+		[]string{"IP", i.IPv4[0].String()},
+		[]string{"Location", i.Region},
+	}
+	tableWidget.Title = "Details"
+
+	instructionsWidget := widgets.NewParagraph()
+	instructionsWidget.Text = "Available actions: (b)oot, (s)hutdown, (l)ist"
+	instructionsWidget.Title = "Instructions"
+
+	v := &LinodeDetailView{
+		linode:             i,
+		parentView:         parent,
+		tableWidget:        tableWidget,
+		instructionsWidget: instructionsWidget,
+	}
+
+	return v
+}
+
+func (v *LinodeDetailView) Grid() *ui.Grid {
+	grid := ui.NewGrid()
+	termWidth, termHeight := ui.TerminalDimensions()
+	grid.SetRect(0, 0, termWidth, termHeight)
+	grid.Set(
+		ui.NewRow(.75,
+			ui.NewCol(1.0, v.tableWidget),
+		),
+		ui.NewRow(.25,
+			ui.NewCol(1.0, v.instructionsWidget),
+		),
+	)
+	return grid
+}
+
+func (v *LinodeDetailView) HandleEvent(ctx context.Context, e ui.Event, ch chan<- View) error {
+	switch e.ID {
+	case "l":
+		ch <- v.parentView
+	default:
+	}
+
+	return nil
+}
+
+type LinodesView struct {
+	linodes     []linodego.Instance
+	linodeViews []*LinodeDetailView
+	listWidget  *widgets.List
+}
+
+func NewLinodesView() *LinodesView {
+	w := widgets.NewList()
+	w.Title = "Linodes"
+	linodes := getLinodes()
+	for _, linode := range linodes {
+		w.Rows = append(w.Rows, linode.Label)
+	}
+	w.TextStyle = ui.NewStyle(ui.ColorBlue)
+	w.WrapText = false
+
+	v := &LinodesView{
+		linodes:    linodes,
+		listWidget: w,
+	}
+
+	views := make([]*LinodeDetailView, len(linodes))
+	for i, linode := range linodes {
+		views[i] = NewLinodeDetailView(v, linode)
+	}
+
+	v.linodeViews = views
+
+	return v
+}
+
+func (v *LinodesView) Grid() *ui.Grid {
+	grid := ui.NewGrid()
+	termWidth, termHeight := ui.TerminalDimensions()
+	grid.SetRect(0, 0, termWidth, termHeight)
+	grid.Set(
+		ui.NewRow(1.0/2,
+			ui.NewCol(1.0/2, v.listWidget),
+		),
+	)
+	return grid
+}
+
+func (v *LinodesView) HandleEvent(ctx context.Context, e ui.Event, next chan<- View) error {
+	switch e.ID {
+	case "j", "<Down>":
+		v.listWidget.ScrollDown()
+	case "k", "<Up>":
+		v.listWidget.ScrollUp()
+	case "<C-d>":
+		v.listWidget.ScrollHalfPageDown()
+	case "<C-u>":
+		v.listWidget.ScrollHalfPageUp()
+	case "<C-f>":
+		v.listWidget.ScrollPageDown()
+	case "<C-b>":
+		v.listWidget.ScrollPageUp()
+	case "<Home>":
+		v.listWidget.ScrollTop()
+	case "G", "<End>":
+		v.listWidget.ScrollBottom()
+	case "<Enter>":
+		idx := v.listWidget.SelectedRow
+		next <- v.linodeViews[idx]
+	default:
+	}
+
+	return nil
+}
+
 func runView(ctx context.Context, view View, events <-chan ui.Event, next chan<- View) error {
-	ticker := time.NewTicker(10*time.Millisecond)
+	ticker := time.NewTicker(10 * time.Millisecond)
 	defer ticker.Stop()
 
 	grid := view.Grid()
-	
+
 	ui.Clear()
 	ui.Render(grid)
 	for {
@@ -97,16 +254,14 @@ func main() {
 	events := ui.PollEvents()
 	ctx, cancel := context.WithCancel(context.Background())
 
-	firstView := NewNamedView("First view")
-	secondView := NewNamedView("Second view")
+	linodesView := NewLinodesView()
 
-	firstView.Next = secondView
-	secondView.Next = firstView
+	// secondView.Next = linodesView
 
 	views := make(chan View)
 
 	go func() {
-		views <- firstView
+		views <- linodesView
 	}()
 
 	for view := range views {
